fb3099ddf3564819e8dc114b32719d367cd7d0e5
karneges <karneges2012@gmail.com>
2022-06-24 18:23:17 +0300

    init

--- src/utils.ts
+++ src/utils.ts
@@ -17,27 +17,6 @@ export const getRandomNonce = () => (Math.random() * 64000) | 0;
 export const zeroAddress = "0:0000000000000000000000000000000000000000000000000000000000000000";
 export const DEPLOY_VALUE = "10000000000000";
 
-export const deployContract = async <Abi extends any, T extends [keyof Contract<Abi>]>(
-  ever: ProviderRpcClient,
-  giver: Giver,
-  abi: Abi,
-  deployParams: GetExpectedAddressParams<Abi>,
-  contractParams: ConstructorParams<Abi>,
-): Promise<Contract<Abi>> => {
-  const expectedAddress = await ever.getExpectedAddress(abi, deployParams);
-  await errorExtractor(giver.sendTo(expectedAddress, DEPLOY_VALUE));
-  const contract = new ever.Contract(abi, expectedAddress);
-  const stateInit = await ever.getStateInit(abi, deployParams);
-  await errorExtractor(
-    contract.methods.constructor(contractParams).sendExternal({
-      stateInit: stateInit.stateInit,
-      publicKey: deployParams.publicKey,
-    }),
-  );
-
-  return contract;
-};
-
 export const errorExtractor = async <T extends { transaction: Transaction<Address>; output?: {} }>(
   transactionResult: Promise<T>,
 ): Promise<T> => {
